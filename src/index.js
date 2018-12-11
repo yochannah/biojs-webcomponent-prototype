892a50a6a90d17c91023a49b5e11d066fa2f6c73
Yo Yehudi <yochannah@gmail.com>
2018-12-11 15:41:11 +0900

    add docs

--- src/index.js
+++ src/index.js
@@ -25,9 +25,12 @@ export const BiojsComponentInteractionGraph = {
   //this function - you'll probably want to update or add content to host.
   render: render(function(host) {
     //Cymine is a function provided by the cytoscape-intermine library dependency
+    //see https://github.com/yochannah/cytoscape-intermine for more details
     Cymine({
       parentElem: host,
       service: {
+        //we're loading dynamic attribute values from our custom element.
+        //see index.html.
         root: host.getAttribute("intermineService")
       },
       queryOn: {
@@ -41,12 +44,12 @@ export const BiojsComponentInteractionGraph = {
     *   It's okay to change the HTML to anything that suits you,
     *   but leave <style-element> here if your
     *   component has its own styles.
+    *   For more info on customising components, see hybrids.js docs
+    *   https://github.com/hybridsjs/hybrids
     **/
     return html `<div>
     <style-element />
     </div>`
-  }, {
-    shadowRoot: false
   })
 }
 
