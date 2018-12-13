8947487ab15acc3147eb797548c8621b90ae15c0
Yo Yehudi <yochannah@gmail.com>
2018-12-13 10:22:38 +0900

    bump cyto version to fix bug thanks to @mb1069

--- src/index.js
+++ src/index.js
@@ -8,26 +8,29 @@ import Cymine from "@intermine/cytoscape-intermine";
 import styles from './style.css';
 
 /**
-* We directly insert a style element in order to embed styles.
-* See https://github.com/hybridsjs/hybrids/issues/10#issuecomment-418340724
-**/
+ * We directly insert a style element in order to embed styles.
+ * See https://github.com/hybridsjs/hybrids/issues/10#issuecomment-418340724
+ **/
 const styleElement = {
   render: html([`<style>${styles}</style`])
 };
 
-/**
-* This is where we place the bulk of the code, wrapping an existing BioJS component
-* or where we might initialise a component written from scratch. Needs to be
-* paired with a `define` method call - see end of the page.
-**/
-export const BiojsComponentInteractionGraph = {
-  //the `host` argument is a reference to the html returned at the bottom of
-  //this function - you'll probably want to update or add content to host.
-  render: render(function(host) {
-    //Cymine is a function provided by the cytoscape-intermine library dependency
-    //see https://github.com/yochannah/cytoscape-intermine for more details
-    Cymine({
-      parentElem: host,
+function connectCymine(options) {
+  //Cymine is a function provided by the cytoscape-intermine library dependency
+  //see https://github.com/yochannah/cytoscape-intermine for more details
+  return {
+    get: (host, v) => v, // required to be recognized as property descriptor,
+    connect: (host, key) => {
+      host.appendChild(document.createElement('div')); // if it is required in your case
+      host[key] = Cymine({
+        ...options,
+        parentElem: host,
+      });
+    };
+  }
+
+  export const BiojsComponentInteractionGraph = {
+    cymine: connectCymine({
       service: {
         //we're loading dynamic attribute values from our custom element.
         //see index.html.
@@ -39,19 +42,15 @@ export const BiojsComponentInteractionGraph = {
       },
       nodeType: host.getAttribute("searchClass"),
       compact: true
-    });
-    /**
-    *   It's okay to change the HTML to anything that suits you,
-    *   but leave <style-element> here if your
-    *   component has its own styles.
-    *   For more info on customising components, see hybrids.js docs
-    *   https://github.com/hybridsjs/hybrids
-    **/
-    return html `<div>
-    <style-element />
-    </div>`
-  }, {shadowRoot:false})
-}
+    }),
+    // your styles should be applied globaly to the document once, so render is not required
+  };
 
-define('biojs-component-interaction-graph', BiojsComponentInteractionGraph);
-define('style-element', styleElement);
+  /**
+   * This is where we place the bulk of the code, wrapping an existing BioJS component
+   * or where we might initialise a component written from scratch. Needs to be
+   * paired with a `define` method call - see end of the page.
+   **/
+
+  define('biojs-component-interaction-graph', BiojsComponentInteractionGraph);
+  define('style-element', styleElement);
